29676aaad06ba40b76ca509d6ccdc5a7e9c8dc30
derabola7 <chidera.a@example.com>
2023-03-19 10:31:19 +0100

    added form

--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -1,14 +1,43 @@
 import React from "react";
+import LoginImg from "../../assets/img/loginAvi.svg";
 
 function Login() {
   return (
     <div className="h-screen bg-gray-50 flex items-center justify-center w-full">
-      <div className="bg-[#f7e2a6] flex shadow-lg">
-      <div className="w-1/2">
+      <div className="flex shadow-lg max-w-3xl">
+        <div className="w-1/2 bg-[#f3ecc5]">
+          <img src={LoginImg} alt="" />
+        </div>
+        <div className="w-1/2 bg-[#1E1E1E] py-2">
+          <h2 className="text-[#fff]">Monthly <span className="text-[#51D289]">Budget</span></h2>
+          <form className="w-full max-w-sm">
+            <div className="flex items-center border-b border-[#fff] py-2">
+              <input
+                className="appearance-none bg-transparent border-none w-50 text-gray-700 py-1 px-2 leading-tight focus:outline-none"
+                type="text"
+                placeholder="insert your income"
+                aria-label="income"
+              />
+            </div>
+            <div className="flex items-center border-b border-[#fff] py-2">
+              <input
+                className="appearance-none bg-transparent border-none w-50 text-gray-700 py-1 px-2 leading-tight focus:outline-none"
+                type="text"
+                placeholder="insert your income"
+                aria-label="income"
+              />
+            </div>
+            <div className="flex items-center border-b border-[#fff] py-2">
+              <input
+                className="appearance-none bg-transparent border-none w-50 text-gray-700 py-1 px-2 leading-tight focus:outline-none"
+                type="text"
+                placeholder="insert your income"
+                aria-label="income"
+              />
+            </div>
+          </form>
+        </div>
       </div>
-      <div className="w-1/2">
-      </div>
-    </div>
     </div>
   );
 }
