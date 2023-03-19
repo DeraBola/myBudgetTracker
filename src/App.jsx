e94fa0cc8dba567ec3cb61ef01d107df9a7276e8
derabola7 <chidera.a@example.com>
2023-03-19 16:38:56 +0100

    added login responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/auth/Login";
+import Home from "./pages/home/Home";
+// import UserContext from "./userContext/UserContext";
 function App() {
   return (
     <div>
       <Router>
         <Routes>
-           <Route exact path="/" element={<Login />} />        
+           <Route exact path="/" element={<Login />} /> 
+           <Route exact path="home" element={<Home />} />       
         </Routes>
       </Router>
     </div>
--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React, {useContext} from "react";
 import LoginImg from "../../assets/img/loginAvi.svg";
-
+// import { UserContext } from "../../userContext/UserContext";
 function Login() {
+  // const user = useContext(UserContext)
   return (
     <div className="h-screen bg-gray-50 flex items-center justify-center w-full">
-      <div className="flex shadow-lg max-w-3xl">
-        <div className="w-1/2 bg-[#f3ecc5]">
+      <div className="flex flex-col lg:flex-row shadow-lg max-w-3xl ">
+        <div className="w-full lg:w-1/2 bg-[#f3ecc5] sm:block hidden">
           <img src={LoginImg} alt="" />
         </div>
-        <div className="w-1/2 bg-[#1E1E1E] flex flex-col p-8 items-center ">
+        <div className="w-full lg:w-1/2 bg-[#1E1E1E] flex flex-col p-8 items-center ">
           <h2 className="text-[#FFF] my-14 mb-8 text-3xl">Monthly <span className="text-[#51D289]">Budget</span></h2>
           <form className="w-full flex flex-col items-center">
             <div className="flex items-center border-b border-[#fff] mb-7">
